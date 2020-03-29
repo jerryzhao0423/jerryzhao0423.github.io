a3a7d9894c6b7f9ce696856b48eae20b3093d56d
Zhejian Zhao <cupidzhaozhejian@hotmail.com>
2020-03-29 11:37:57 -0400

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-## JerryZhao's Personal Blog
-
-This is a simple personal website. 
-
-### Website Address 
-https://jerryzhao0423.github.io/
-
-### Run Local
-``` bundle exec jekyll serve```: Default port - http://localhost:4000
---
+++ precache-manifest.14cefe461b742d9062870c4b0f34c4e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b776e37c9c892d2ad7a5a123ac4f300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e3ad1aab80809084a1e",
+    "url": "/static/css/main.c5898adf.chunk.css"
+  },
+  {
+    "revision": "cd68aa1934257275ee38",
+    "url": "/static/js/2.4a563f8e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.4a563f8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e3ad1aab80809084a1e",
+    "url": "/static/js/main.df5e71a9.chunk.js"
+  },
+  {
+    "revision": "0dda34acc7b81aad8062",
+    "url": "/static/js/runtime-main.c399816e.js"
+  }
+]);
